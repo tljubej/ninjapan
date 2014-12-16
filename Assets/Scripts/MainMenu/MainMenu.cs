4c4bbe301225a3104d5306d7b5caa86da41f5813
Filip Benasic <filip.benasic@gmail.com>
2014-12-16 23:41:17 +0100

    Added basic play and quit button

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -2,14 +2,12 @@
 using System.Collections;
 
 public class MainMenu : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
 	
+	public void StartGame () {
+		Application.LoadLevel("Level1");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void ExitGame(){
+		Application.Quit();
 	}
 }
